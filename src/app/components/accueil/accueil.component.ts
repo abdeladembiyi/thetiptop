beeb62bf8ebf553578bd4ae72d3158e827e0bfc4
Mabdeljalil <abdeljalil.elmehdi97@gmail.com>
2023-12-12 01:57:16 +0100

    first responsivness push

--- src/app/components/accueil/accueil.component.ts
+++ src/app/components/accueil/accueil.component.ts
@@ -3,7 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-accueil',
   templateUrl: './accueil.component.html',
-  styleUrls: ['./accueil.component.css']
+  styleUrls: ['./accueil.component.css', './responsive.component.css' ]
+  
 })
 export class AccueilComponent {
 
